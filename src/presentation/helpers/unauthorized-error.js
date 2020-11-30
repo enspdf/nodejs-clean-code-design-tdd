5da7c9f06e4f0694fa3c7dc50505058de65e04d6
Sebastian Higuita <sebastianhiguita@Sebastians-MacBook-Air.local>
2020-11-29 20:18:55 -0500

    Create custom response, errors and adjust tests

---
+++ src/presentation/helpers/unauthorized-error.js
@@ -0,0 +1,8 @@
+module.exports = class UnauthorizedError extends (
+  Error
+) {
+  constructor() {
+    super("Unauthorized");
+    this.name = "UnauthorizedError";
+  }
+};
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,28 @@
 const LoginRouter = require("./login-router");
 const MissingParamError = require("../helpers/missing-param-error");
+const UnauthorizedError = require("../helpers/unauthorized-error");
+
+const makeSut = () => {
+  class AuthCaseSpy {
+    auth(email, password) {
+      this.email = email;
+      this.password = password;
+    }
+  }
+
+  const authUseCaseSpy = new AuthCaseSpy();
+
+  const sut = new LoginRouter(authUseCaseSpy);
+
+  return {
+    sut,
+    authUseCaseSpy,
+  };
+};
 
 describe("Login Router", () => {
   test("Should return 400 if email is not provided", () => {
-    const login = new LoginRouter();
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -11,14 +30,14 @@ describe("Login Router", () => {
       },
     };
 
-    const httpResponse = login.route(httpRequest);
+    const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
 
   test("Should return 400 if password is not provided", () => {
-    const login = new LoginRouter();
+    const { sut } = makeSut();
 
     const httpRequest = {
       body: {
@@ -26,25 +45,57 @@ describe("Login Router", () => {
       },
     };
 
-    const httpResponse = login.route(httpRequest);
+    const httpResponse = sut.route(httpRequest);
 
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
 
   test("Should return 500 if httpRequest is not provided", () => {
-    const login = new LoginRouter();
+    const { sut } = makeSut();
 
-    const httpResponse = login.route();
+    const httpResponse = sut.route();
 
     expect(httpResponse.statusCode).toBe(500);
   });
 
   test("Should return 500 if httpRequest has no body", () => {
-    const login = new LoginRouter();
+    const { sut } = makeSut();
 
-    const httpResponse = login.route({});
+    const httpResponse = sut.route({});
 
     expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test("Should call AuthUseCase with correct params", () => {
+    const { sut, authUseCaseSpy } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+      },
+    };
+
+    sut.route(httpRequest);
+
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
+  });
+
+  test("Should return 401 when invalid credentials are provided", () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        email: "invalid_email@mail.com",
+        password: "invalid_password",
+      },
+    };
+
+    const httpResponse = sut.route(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(401);
+    expect(httpResponse.body).toEqual(new UnauthorizedError());
   });
 });
